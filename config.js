1e0d7fe6265be6f83e41d9095ea1f4ecbc540adc
THMEO-X <htgaming7811@gmail.com>
2026-08-11 18:48:14 +0700

    Update LARGE_IMAGE_URL in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   APPLICATION_ID: "1460999009421230090",
 
   LARGE_IMAGE_URL:
-    "https://cdn.discordapp.com/attachments/1336313310542561300/1342117509847056464/image0.gif?size=4096",
+    "https://cdn.discordapp.com/avatars/1377219236438741073/a_ce988ec5d8c482dd14421131a30357ba.gif?size=4096",
 
   BUTTON_1_LABEL: "website",
   BUTTON_1_URL: "https://gem-50fe.onrender.com/",
