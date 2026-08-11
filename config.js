0304ac782ee42e77bb99982e6c70f4098181ba85
THMEO-X <htgaming7811@gmail.com>
2026-08-11 17:00:30 +0700

    Update button labels and URLs in config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ module.exports = {
   LARGE_IMAGE_URL:
     "https://cdn.discordapp.com/avatars/1390674997777010770/0b55fd397f1bf25834fc3c19586f2c4a.png?size=4096",
 
-  BUTTON_1_LABEL: "Github",
-  BUTTON_1_URL: "https://github.com/THMEO-X/custom-xx",
+  BUTTON_1_LABEL: "website",
+  BUTTON_1_URL: "https://gem-50fe.onrender.com/",
 
-  BUTTON_2_LABEL: "Discord Sever",
-  BUTTON_2_URL: "https://discord.gg/VgqRgUgKzM",
+  BUTTON_2_LABEL: "xyz",
+  BUTTON_2_URL: "https://Google.com",
 };
